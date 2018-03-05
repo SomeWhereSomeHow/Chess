7f98b61072ccd94a19e031688ac0ecb3fa065f25
Lukas Adam <lukas.adam@gmx.de>
2018-03-05 23:28:40 +0100

    some stuff

---
+++ Chess/Figures/Figures.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Chess.Figures
+{
+    public interface Figure
+    {
+        Position position;
+
+        Color color;
+
+        bool Move();
+    }
+}
+
+public enum Color {Black, White}
---
+++ Chess/Figures/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Chess.Figures
+{
+    public class Position
+    {
+        public Position()
+        {
+        }
+
+        public Position( int x, int y){
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; };
+        public int Y { get; set; };
+    }
+}
